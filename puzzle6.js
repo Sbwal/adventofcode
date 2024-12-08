e051fce7783932c096620b440ab761c5f64bbb28
Shubham Baranwal <shubhambaranwal83@gmail.com>
2024-12-08 13:30:33 +0530

    setting aside puzzle 6 day 2

--- puzzle6.js
+++ puzzle6.js
@@ -1,23 +1,12 @@
 const { readFile } = require("./helper")
-
-const puzzleInputFile = 'puzzle6.txt';
+const puzzleInputFile = 'test.txt';
 
 const code = async () => {
     let data = await readFile(puzzleInputFile);
     data = data.split('\r\n').map(v => v.split(''));
     const n = data.length, m = data[0].length;
-    const dir = {
-        '>': 'v',
-        'v': '<',
-        '<': '^',
-        '^': '>'
-    }
-    const move = {
-        '>': [0, 1],
-        'v': [1, 0],
-        '<': [0, -1],
-        '^': [-1, 0]
-    }
+    const dir = {'>': 'v','v': '<','<': '^','^': '>'}
+    const move = {'>': [0, 1],'v': [1, 0],'<': [0, -1],'^': [-1, 0]}
     const find = () => {
         for (let i = 0; i < n; i++) {
             for (let j = 0; j < m; j++) {
@@ -27,10 +16,9 @@ const code = async () => {
             }
         }
     }
-    const inValid = (im, jm) => {
-        return im >= n || im < 0 || jm >= m || jm < 0;
-    }
-    const returnBack = (x, y, pd) => {
+    const inValid = (im, jm) => im >= n || im < 0 || jm >= m || jm < 0;
+    const returnBack = (x, y, pdo) => {
+        let pd = pdo;
         const map = Array(n).fill().map(_ => Array(m).fill(0));
         let i = x, j = y;
         let c = 0;
@@ -41,8 +29,7 @@ const code = async () => {
                 return false;
             }
             map[im][jm]++;
-            if(map[im][jm] > 5) {
-                console.log('repeated')
+            if (map[im][jm] > 4) {
                 return false;
             }
             if (data[im][jm] === '#' || data[im][jm] === 'o') {
@@ -51,14 +38,17 @@ const code = async () => {
             }
             i = im;
             j = jm;
-            if(x === i && y === j) {
+            if (x === i && y === j) {
                 return true;
             }
         }
     }
     let [i, j] = find();
     let pd = data[i][j];
+    data[i][j] = '.';
     let c = 0;
+    const otrue = Array(n).fill().map(_ => Array(m).fill(false));
+    const op = Array(n).fill().map(_ => Array(m).fill(false));
     while (true) {
         let im = i + move[pd][0];
         let jm = j + move[pd][1];
@@ -69,32 +59,32 @@ const code = async () => {
             pd = dir[pd];
             continue;
         }
-        data[im][jm] = 'o';
-        // console.clear();
-        // process.stdout.write('\x1Bc')
-        // console.log('\n\n\n&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
-        // console.log(data.map(v => v.join('')).join('\n'));
-        const val = returnBack(i, j, pd);
-        if(val) {
-            c++;
+        if (!otrue[im][jm]) {
+            otrue[im][jm] = true;
+            data[im][jm] = 'o';
+            const val = returnBack(i, j, pd);
+            if (val) {
+                op[i][j] = true;
+                c++;
+            }
+            data[im][jm] = '.';
         }
-        data[im][jm] = '.';
         i = im;
         j = jm;
     }
-
-    // console.log(data.map(v => v.join('')).join('\n'));
+    for(let i = 0; i < n; i++) {
+        let s = '';
+        for(let j = 0; j < m; j++) {
+            if(op[i][j]) {
+                s += '0';
+            }
+            else {
+                s += data[i][j];
+            }
+        }
+        console.log(s)
+    }
     console.log(c)
-
-    // let c = 0;
-    // for (i = 0; i < n; i++) {
-    //     for (j = 0; j < m; j++) {
-    //         if (data[i][j] === 'x') {
-    //             c++;
-    //         }
-    //     }
-    // }
-    // console.log(c)
 
 }
 code();
